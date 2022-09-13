de5669a25d8a0832d451bc5ae39f290ce9c934ce
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-09-13 15:10:32 +0700

    feat: implement everything of sidebar

--- src/component/Sidebar/index.tsx
+++ src/component/Sidebar/index.tsx
@@ -18,9 +18,15 @@ import {
   MdOutlinePowerSettingsNew,
   MdKeyboardArrowDown,
   MdInsertChartOutlined,
-  MdOutlineDashboard,
+  MdStorefront,
 } from "react-icons/md";
-
+import { FaRobot, FaNewspaper } from "react-icons/fa";
+import { BsShieldFillPlus, BsFillKeyFill } from "react-icons/bs";
+import { RiFolderUserLine, RiBillLine } from "react-icons/ri";
+import { TiFlowChildren } from "react-icons/ti";
+import { TbAlertCircle } from "react-icons/tb";
+import { GiPencilRuler } from "react-icons/gi";
+import { IoMdNuclear } from "react-icons/io";
 import avatar from "../../assets/img/avatar.jpg";
 import style from "./sidebar.module.css";
 
@@ -32,45 +38,322 @@ export default function Sidebar() {
 
   const user: User = useSelector(loginSelectors.selectAccount);
 
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState("");
   const [activeItem, setActiveItem] = useState("");
 
   const sidebarNavItems = [
     {
-      display: t("SIDEBAR.APPLICATION"),
-      icon: <MdInsertChartOutlined></MdInsertChartOutlined>,
-      selectIndex: 1,
-      children: [
+      field: "GENERAL",
+      field_child: [
         {
-          display: t("SIDEBAR.MAIL"),
-          to: `/mail`,
+          display: "Blueprints",
+          icon: <FaNewspaper></FaNewspaper>,
+          children: [
+            {
+              display: "Extended sidebar",
+              to: `/calendar`,
+            },
+            {
+              display: "Accent header",
+              to: `/file`,
+            },
+            {
+              display: "Accent sidebar",
+              to: `/job`,
+            },
+            {
+              display: "Boxed sidebar",
+              to: `/mail`,
+            },
+            {
+              display: "Collapsed sidebar",
+              to: `/messenger`,
+            },
+            {
+              display: "Bottom navigation",
+              to: `/project`,
+            },
+            {
+              display: "Top navigation",
+              to: `/project`,
+            },
+          ],
         },
         {
-          display: t("SIDEBAR.CALENDAR"),
-          to: `/calendar`,
+          display: t("SIDEBAR.DASHBOARD"),
+          icon: <FaRobot></FaRobot>,
+          children: [
+            {
+              display: t("SIDEBAR.REPORTS"),
+              to: `/report`,
+            },
+            {
+              display: "Expenses",
+              to: `/Expenses`,
+            },
+            {
+              display: t("SIDEBAR.PRODUCTS"),
+              to: `/product`,
+            },
+            {
+              display: "Statistics",
+              to: `/Statistics`,
+            },
+            {
+              display: "Automation",
+              to: `/Automation`,
+            },
+            {
+              display: "Analytics",
+              to: `/Analytics`,
+            },
+            {
+              display: "Banking",
+              to: `/Banking`,
+            },
+            {
+              display: t("SIDEBAR.CRYPTO"),
+              to: `/crypto`,
+            },
+            {
+              display: "Finance",
+              to: `/Finance`,
+            },
+            {
+              display: "Fitness",
+              to: `/Fitness`,
+            },
+          ],
         },
         {
-          display: t("SIDEBAR.FILE"),
-          to: `/file`,
+          display: "Data Display",
+          icon: <BsShieldFillPlus></BsShieldFillPlus>,
+          children: [
+            {
+              display: t("SIDEBAR.REPORTS"),
+              to: `/report`,
+            },
+            {
+              display: "Expenses",
+              to: `/Expenses`,
+            },
+            {
+              display: t("SIDEBAR.PRODUCTS"),
+              to: `/product`,
+            },
+            {
+              display: "Statistics",
+              to: `/Statistics`,
+            },
+            {
+              display: "Automation",
+              to: `/Automation`,
+            },
+            {
+              display: "Analytics",
+              to: `/Analytics`,
+            },
+            {
+              display: "Banking",
+              to: `/Banking`,
+            },
+            {
+              display: t("SIDEBAR.CRYPTO"),
+              to: `/crypto`,
+            },
+            {
+              display: "Finance",
+              to: `/Finance`,
+            },
+            {
+              display: "Fitness",
+              to: `/Fitness`,
+            },
+          ],
+        },
+        {
+          display: t("SIDEBAR.APPLICATION"),
+          icon: <MdInsertChartOutlined></MdInsertChartOutlined>,
+          children: [
+            {
+              display: "Charts large",
+              to: `/calendar`,
+            },
+            {
+              display: "Charts small",
+              to: `/file`,
+            },
+            {
+              display: "Component cards",
+              to: `/job`,
+            },
+            {
+              display: "Grid",
+              to: `/mail`,
+            },
+
+            {
+              display: "Icon cards",
+              to: `/messenger`,
+            },
+
+            {
+              display: "Image cards",
+              to: `/project`,
+            },
+            {
+              display: "Lists large",
+              to: `/project`,
+            },
+            {
+              display: "Navigation",
+              to: `/project`,
+            },
+            {
+              display: "Profile cards",
+              to: `/project`,
+            },
+            {
+              display: "Progress circular",
+              to: `/project`,
+            },
+            {
+              display: "Progress horizontal",
+              to: `/project`,
+            },
+            {
+              display: "Sparklines large",
+              to: `/project`,
+            },
+            {
+              display: "Sparklines small",
+              to: `/project`,
+            },
+            {
+              display: "Statistics",
+              to: `/project`,
+            },
+          ],
         },
       ],
     },
+
     {
-      display: t("SIDEBAR.DASHBOARD"),
-      icon: <MdOutlineDashboard></MdOutlineDashboard>,
-      selectIndex: 2,
-      children: [
+      field: "MANAGEMENT",
+      field_child: [
         {
-          display: t("SIDEBAR.REPORTS"),
-          to: `/report`,
+          display: "Users",
+          icon: <RiFolderUserLine></RiFolderUserLine>,
+          children: [
+            {
+              display: "List",
+              to: `/calendar`,
+            },
+            {
+              display: "User Profile",
+              to: `/file`,
+            },
+          ],
         },
         {
-          display: t("SIDEBAR.PRODUCTS"),
-          to: `/product`,
+          display: "Projects",
+          icon: <TiFlowChildren></TiFlowChildren>,
         },
         {
-          display: t("SIDEBAR.CRYPTO"),
-          to: `/crypto`,
+          display: "Commerce",
+          icon: <MdStorefront></MdStorefront>,
+          children: [
+            {
+              display: "Shop",
+              to: `/report`,
+            },
+            {
+              display: "List",
+              to: `/Expenses`,
+            },
+            {
+              display: "Details",
+              to: `/product`,
+            },
+            {
+              display: "Create",
+              to: `/Statistics`,
+            },
+          ],
+        },
+        {
+          display: "Invoices",
+          icon: <RiBillLine></RiBillLine>,
+          children: [
+            {
+              display: "List",
+              to: `/calendar`,
+            },
+            {
+              display: "Details",
+              to: `/file`,
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      field: "EXTRA PAGES",
+      field_child: [
+        {
+          display: "Auth Pages",
+          icon: <BsFillKeyFill></BsFillKeyFill>,
+          children: [
+            {
+              display: "Login",
+              to: `/calendar`,
+            },
+            {
+              display: "Register",
+              to: `/file`,
+            },
+            {
+              display: "Recover Password",
+              to: `/file`,
+            },
+          ],
+        },
+        {
+          display: "Status",
+          icon: <TbAlertCircle></TbAlertCircle>,
+          children: [
+            {
+              display: "Error 404",
+              to: `/calendar`,
+            },
+            {
+              display: "Error 500",
+              to: `/file`,
+            },
+            {
+              display: "Maintanance",
+              to: `/file`,
+            },
+            {
+              display: "Comming Soon",
+              to: `/file`,
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      field: "FOUNDATION",
+      field_child: [
+        {
+          display: "Overview",
+          icon: <GiPencilRuler></GiPencilRuler>,
+        },
+        {
+          display: "Documentation",
+          icon: <IoMdNuclear></IoMdNuclear>,
         },
       ],
     },
@@ -78,13 +361,16 @@ export default function Sidebar() {
 
   useEffect(() => {
     let address = "";
-    sidebarNavItems.forEach((item) => {
-      item.children.forEach((child) => {
-        if (location.pathname.includes(child.to)) {
-          address = child.display;
-        }
+    sidebarNavItems.forEach((i) => {
+      i.field_child.forEach((item) => {
+        item?.children?.forEach((child) => {
+          if (location.pathname.includes(child.to)) {
+            address = child.display;
+          }
+        });
       });
     });
+
     setActiveItem(address);
   }, []);
 
@@ -95,97 +381,108 @@ export default function Sidebar() {
   };
 
   return (
-    <Box
-      sx={{
-        width: "300px",
-        height: "100vh",
-        backgroundColor: "#11192a",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        padding: "40px 30px",
-      }}
-    >
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <img style={{ borderRadius: "100%" }} src={avatar}></img>
-        <Typography
-          fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
-          variant="body1"
-          color="white"
-          marginTop="10px"
+    <Box className={style.sidebar}>
+      <Box className={style.sidebar_body}>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          {user.email}
-        </Typography>
-
-        <Typography
-          fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
-          variant="body1"
-          color="rgba(255, 255, 255, 0.7)"
-        >
-          Technical Manager
-        </Typography>
-      </Box>
-
-      <Box
-        sx={{
-          width: "100%",
-          height: "1px",
-          backgroundColor: "rgba(255, 255, 255, 0.1)",
-          marginTop: "30px",
-        }}
-      ></Box>
-      <Box
-        sx={{
-          marginTop: "30px",
-        }}
-      >
-        <Box className={style.field_title}>{t("SIDEBAR.GENERAL")}</Box>
-
-        {sidebarNavItems.map((item, index) => (
-          <Accordion
-            key={index}
-            className={`${style.customAccordion}`}
-            onChange={(e, expanded) => {
-              if (expanded) setActiveIndex(index + 1);
-              else setActiveIndex(0);
-            }}
+          <img style={{ borderRadius: "100%" }} src={avatar}></img>
+          <Typography
+            fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
+            variant="body1"
+            color="white"
+            marginTop="10px"
           >
-            <AccordionSummary
-              expandIcon={
-                <MdKeyboardArrowDown className={style.sidebar_expandIcon} />
-              }
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-              className={`${style.sidebar_item} ${
-                activeIndex === item.selectIndex && style.active
-              } `}
+            {user.email}
+          </Typography>
+
+          <Typography
+            fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
+            variant="body1"
+            color="rgba(255, 255, 255, 0.7)"
+          >
+            Technical Manager
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            width: "100%",
+            height: "1px",
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            marginTop: "30px",
+          }}
+        ></Box>
+        <Box
+          sx={{
+            marginTop: "30px",
+          }}
+        >
+          {sidebarNavItems.map((sideItem) => (
+            <Box
+              sx={{
+                marginBottom: "30px",
+              }}
             >
-              <Box className={style.sidebar_icon}>{item.icon}</Box>
-              <Box className={style.sidebar_text}>{item.display}</Box>
-            </AccordionSummary>
-            <AccordionDetails>
-              {item.children.map((child) => (
-                <Box
-                  sx={{ paddingLeft: "23px" }}
-                  className={`${style.accordion_item} ${
-                    activeItem === child.display && style.active_accordion_item
-                  }  `}
-                  onClick={() => handleActiveItem(child.to, child.display)}
+              <Box className={style.field_title}>{sideItem.field}</Box>
+
+              {sideItem.field_child.map((item, index) => (
+                <Accordion
+                  key={index}
+                  className={`${style.customAccordion}`}
+                  onChange={(e, expanded) => {
+                    if (expanded) setActiveIndex(item.display);
+                    else setActiveIndex("");
+                  }}
                 >
-                  <Box className={style.accordion_dot}></Box>
-                  <Box className={style.accordion_text}>{child.display}</Box>
-                </Box>
+                  <AccordionSummary
+                    expandIcon={
+                      item.children ? (
+                        <MdKeyboardArrowDown
+                          className={style.sidebar_expandIcon}
+                        />
+                      ) : (
+                        ""
+                      )
+                    }
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    className={`${style.sidebar_item} ${
+                      activeIndex === item.display && style.active
+                    } `}
+                  >
+                    <Box className={style.sidebar_icon}>{item.icon}</Box>
+                    <Box className={style.sidebar_text}>{item.display}</Box>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    {item?.children?.map((child) => (
+                      <Box
+                        sx={{ paddingLeft: "23px" }}
+                        className={`${style.accordion_item} ${
+                          activeItem === child.display &&
+                          style.active_accordion_item
+                        }  `}
+                        onClick={() =>
+                          handleActiveItem(child.to, child.display)
+                        }
+                      >
+                        <Box className={style.accordion_dot}></Box>
+                        <Box className={style.accordion_text}>
+                          {child.display}
+                        </Box>
+                      </Box>
+                    ))}
+                  </AccordionDetails>
+                </Accordion>
               ))}
-            </AccordionDetails>
-          </Accordion>
-        ))}
+            </Box>
+          ))}
+        </Box>
       </Box>
 
       <Box className={style.sidebar_footer}>
