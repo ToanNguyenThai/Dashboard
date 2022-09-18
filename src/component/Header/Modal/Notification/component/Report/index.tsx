406e09063ece5a15e029e25d449b6bdedf9592c1
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-09-18 11:20:13 +0700

    feat: complete noti modal

--- src/component/Header/Modal/Notification/component/Report/index.tsx
+++ src/component/Header/Modal/Notification/component/Report/index.tsx
@@ -1,5 +1,80 @@
-import React from "react";
+import { useState } from "react";
+import { Box, Typography } from "@mui/material";
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+import style from "./report.module.css";
+const options: ApexOptions = {
+  chart: {
+    stacked: true,
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: true,
+    },
+  },
 
+  grid: {
+    show: false,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      borderRadius: 10,
+    },
+  },
+  xaxis: {
+    labels: {
+      show: false,
+    },
+    /* remove bottom ruler */
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+    /* remove small tooltip */
+  },
+  yaxis: {
+    show: false,
+  },
+  legend: {
+    show: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  colors: ["#999ebb", "#585d7f"],
+};
 export default function Report() {
-  return <div>Report</div>;
+  const [state, setState] = useState({
+    series: [
+      {
+        name: "Net Profit",
+        data: [2.3, 3.1, 4, 3.8, 5.1, 3.6, 4, 3.8, 5.1, 3.6, 0],
+      },
+      {
+        name: "Net Lost",
+        data: [2.1, 2.1, 3, 2.8, 4, 3.8, 5.1, 3.6, 4.1, 2.6, 1.2],
+      },
+    ],
+    options: options,
+  });
+  return (
+    <Box className={style.report}>
+      <Typography fontSize="16px" fontWeight="600" textAlign="center">
+        Total Sales
+      </Typography>
+      <Typography fontSize="15px" color="#223354B3" textAlign="center">
+        Total sales performance for last week
+      </Typography>
+      <ReactApexChart
+        options={state.options}
+        series={state.series}
+        type="bar"
+        height={200}
+      />
+    </Box>
+  );
 }
--- src/component/Header/Modal/Notification/component/Task/index.tsx
+++ src/component/Header/Modal/Notification/component/Task/index.tsx
@@ -1,5 +1,54 @@
-import React from "react";
+import { Typography, Box, Card, CardContent } from "@mui/material";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { FiCheck } from "react-icons/fi";
 
+import style from "./task.module.css";
 export default function Task() {
-  return <div>Task</div>;
+  return (
+    <Box className={style.task}>
+      <Typography
+        fontSize="13px"
+        fontWeight="700"
+        color="rgba(34, 51, 84, 0.5)"
+        textTransform="uppercase"
+      >
+        Task for today
+      </Typography>
+      <Card className={style.task_card}>
+        <CardContent>
+          <Typography fontSize="16px">Presentation site design</Typography>
+          <Box sx={{ display: "flex", marginTop: "10px" }}>
+            <Box className={style.onHold_btn}>on hold</Box>
+            <Box className={style.time}>
+              <AiOutlineClockCircle fontSize="18px"></AiOutlineClockCircle>
+              <Typography fontSize="14px" marginLeft="5px">
+                2:35 pm
+              </Typography>
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
+
+      <Typography
+        fontSize="13px"
+        fontWeight="700"
+        color="rgba(34, 51, 84, 0.5)"
+        textTransform="uppercase"
+        marginTop="25px"
+      >
+        TOMORROW'S SCHEDULE
+      </Typography>
+      <Box className={style.circle_container}>
+        <Box className={style.circle}>
+          <FiCheck color="#57ca22" fontSize="40px"></FiCheck>
+        </Box>
+        <Typography fontSize="16px" fontWeight="600">
+          Nothing to report
+        </Typography>
+        <Typography fontSize="15px" color="#223354B3">
+          You don't have any other pending tasks in progress!
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
