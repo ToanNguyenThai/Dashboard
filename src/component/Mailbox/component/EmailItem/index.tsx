c2fc5ad4bd6a1bac17013cf9012d43b954409fcd
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-09-23 11:06:45 +0700

    fix: email item UI

--- src/component/Mailbox/component/EmailItem/index.tsx
+++ src/component/Mailbox/component/EmailItem/index.tsx
@@ -66,8 +66,6 @@ export default function EmailItem({
         borderBottom: "1px solid #e9e9e9",
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-between",
-        cursor: "pointer",
       }}
       className={
         selected || emailItem.checked ? style.selectedBG : style.normalBG
@@ -81,126 +79,157 @@ export default function EmailItem({
           onChange={(e) => handleCheckbox(e)}
         />
       </Box>
-      <Box
-        sx={{
-          display: "flex",
-          width: "7%",
-        }}
-      >
-        {email.important && (
-          <Box className={style.sidebar_icon} sx={{ color: "#57ca22" }}>
-            <FaTag />
-          </Box>
-        )}{" "}
-        {email.work && (
-          <Box className={style.sidebar_icon} sx={{ color: "#ffa319" }}>
-            <FaTag />
-          </Box>
-        )}{" "}
-        {email.task && (
-          <Box className={style.sidebar_icon} sx={{ color: "#33c2ff" }}>
-            <FaTag />
-          </Box>
-        )}{" "}
-        {email.business && (
-          <Box className={style.sidebar_icon} sx={{ color: "#ff1943" }}>
-            <FaTag />
-          </Box>
-        )}{" "}
-      </Box>
 
       <Box
         sx={{
-          width: "20%",
+          width: "100%",
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
         }}
-        onClick={() => navigate(`/mail/details/${email.id}`)}
       >
-        <img src={avatar} className={style.avatar}></img>
-        <Typography
-          fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
-          variant="h6"
-          fontSize="16px"
-          fontWeight="600"
-        >
-          {email.from}
-        </Typography>
-      </Box>
-
-      <Box
-        sx={{
-          width: "45%",
-        }}
-        onClick={() => navigate(`/mail/details/${email.id}`)}
-      >
-        <Typography
-          fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
-          variant="h6"
-          fontSize="15px"
-          fontWeight="600"
-        >
-          {email.title}
-        </Typography>
-        <Typography
-          fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
-          variant="body1"
-          fontSize="14px"
-          color="#223354B3"
-        >
-          {email.content
-            .split(" ")
-            .slice(0, 13)
-            .join(" ")
-            .replace(/<[^>]*>?/gm, "")}
-        </Typography>
-      </Box>
-
-      {!isHover && !emailItem.checked ? (
-        <Typography
-          width="12%"
-          variant="body1"
-          fontSize="14px"
-          color="#223354B3"
-          textAlign="right"
-        >
-          {email.day.split(" ").slice(0, 2).join(" ")}
-        </Typography>
-      ) : (
         <Box
           sx={{
             display: "flex",
             alignItems: "center",
-            justifyContent: "space-between",
-            padding: "0px 18px",
-            width: "12%",
-            height: "55px",
-            backgroundColor: "white",
-            boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px;",
-            borderRadius: "10px",
+            width: "70%",
+            marginLeft: "10px",
           }}
         >
-          <CustomWidthTooltip title="Archive" arrow placement="top">
-            <Box className={style.icon}>
-              <MdArchive fontSize="23px" />
-            </Box>
-          </CustomWidthTooltip>
+          <Box
+            sx={{
+              display: "flex",
+              width: "9.5%",
+            }}
+          >
+            {email.important && (
+              <CustomWidthTooltip title="Important" arrow placement="top">
+                <Box className={style.sidebar_icon} sx={{ color: "#57ca22" }}>
+                  <FaTag />
+                </Box>
+              </CustomWidthTooltip>
+            )}{" "}
+            {email.work && (
+              <CustomWidthTooltip title="Work" arrow placement="top">
+                <Box className={style.sidebar_icon} sx={{ color: "#ffa319" }}>
+                  <FaTag />
+                </Box>
+              </CustomWidthTooltip>
+            )}{" "}
+            {email.task && (
+              <CustomWidthTooltip title="Task" arrow placement="top">
+                <Box className={style.sidebar_icon} sx={{ color: "#33c2ff" }}>
+                  <FaTag />
+                </Box>
+              </CustomWidthTooltip>
+            )}{" "}
+            {email.business && (
+              <CustomWidthTooltip title="Business" arrow placement="top">
+                <Box className={style.sidebar_icon} sx={{ color: "#ff1943" }}>
+                  <FaTag />
+                </Box>
+              </CustomWidthTooltip>
+            )}{" "}
+          </Box>
 
-          <CustomWidthTooltip title="Delete" arrow placement="top">
-            <Box
-              onClick={() => dispatch(emailActions.delete_single(emailItem.id))}
-              className={style.icon}
+          <Box
+            sx={{
+              width: "22%",
+              display: "flex",
+              alignItems: "center",
+              cursor: "pointer",
+            }}
+            onClick={() => navigate(`/mail/details/${email.id}`)}
+          >
+            <img src={avatar} className={style.avatar}></img>
+            <Typography
+              variant="h6"
+              fontSize="14px"
+              fontWeight="600"
+              color="#223354"
             >
-              <FaRegTrashAlt fontSize="20px" />
-            </Box>
-          </CustomWidthTooltip>
-          <CustomWidthTooltip title="Mark as read" arrow placement="top">
-            <Box className={style.icon}>
-              <RiMailCheckFill fontSize="21px" />
-            </Box>
-          </CustomWidthTooltip>
+              {email.from}
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              width: "60%",
+              marginLeft: "30px",
+              cursor: "pointer",
+            }}
+            onClick={() => navigate(`/mail/details/${email.id}`)}
+          >
+            <Typography
+              variant="h6"
+              fontSize="14px"
+              fontWeight="600"
+              color="#223354"
+            >
+              {email.title}
+            </Typography>
+            <Typography
+              fontFamily={`"Segoe UI", Tahoma, Geneva, Verdana, sans-serif`}
+              variant="body1"
+              fontSize="14px"
+              color="#223354B3"
+            >
+              {email.content
+                .split(" ")
+                .slice(0, 13)
+                .join(" ")
+                .replace(/<[^>]*>?/gm, "")}
+            </Typography>
+          </Box>
         </Box>
-      )}
+        {!isHover && !emailItem.checked ? (
+          <Typography
+            width="12%"
+            variant="body1"
+            fontSize="14px"
+            color="#223354B3"
+            textAlign="right"
+          >
+            {email.day.split(" ").slice(0, 2).join(" ")}
+          </Typography>
+        ) : (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "0px 18px",
+              width: "12%",
+              height: "55px",
+              backgroundColor: "white",
+              boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px;",
+              borderRadius: "10px",
+            }}
+          >
+            <CustomWidthTooltip title="Archive" arrow placement="top">
+              <Box className={style.icon}>
+                <MdArchive fontSize="23px" />
+              </Box>
+            </CustomWidthTooltip>
+
+            <CustomWidthTooltip title="Delete" arrow placement="top">
+              <Box
+                onClick={() =>
+                  dispatch(emailActions.delete_single(emailItem.id))
+                }
+                className={style.icon}
+              >
+                <FaRegTrashAlt fontSize="20px" />
+              </Box>
+            </CustomWidthTooltip>
+            <CustomWidthTooltip title="Mark as read" arrow placement="top">
+              <Box className={style.icon}>
+                <RiMailCheckFill fontSize="21px" />
+              </Box>
+            </CustomWidthTooltip>
+          </Box>
+        )}
+      </Box>
     </Box>
   );
 }
