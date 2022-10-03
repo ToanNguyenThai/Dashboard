6c660d4a7dd6ccd15e4c6d0fb5f50700b203620d
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-10-03 15:20:12 +0700

    feat: implement drag n drop file

--- src/component/AddProduct/RightLayout/Image/index.tsx
+++ src/component/AddProduct/RightLayout/Image/index.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
 import { Box, Typography } from "@mui/material";
 import { PreviewCard } from "../../../UI-components/PreviewCard";
-import { MdOutlineCloudUpload } from "react-icons/md";
+import {
+  MdOutlineCloudUpload,
+  MdCheck,
+  MdOutlineCheckCircleOutline,
+} from "react-icons/md";
 import style from "../rightlayout.module.css";
+
 export default function Image() {
+  const [file, setFile] = useState<File | null>(null);
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    accept: {
+      "image/*": [".jpeg", ".png", ".jpg"],
+    },
+    onDrop: (acceptedFile) => {
+      console.log(acceptedFile[0]);
+
+      setFile(acceptedFile[0]);
+    },
+  });
   return (
     <PreviewCard sx={{ marginTop: "25px" }}>
       <Box
@@ -25,27 +49,77 @@ export default function Image() {
           borderTop: "1px solid #d5d8dc",
         }}
       >
-        <input
-          accept="image/*"
-          id="messenger-upload-file"
-          type="file"
-          style={{ display: "none" }}
-        ></input>
-        <label
-          htmlFor="messenger-upload-file"
-          aria-label="Attach a file"
-          className={style.uploadContainer}
-        >
-          <MdOutlineCloudUpload
-            style={{ transform: "translateY(17px)" }}
-            color="#5569ff "
-            fontSize="28px"
-          ></MdOutlineCloudUpload>
-          <Typography fontSize="13px" color="#223354" fontWeight="500">
-            Drag drop files here
-          </Typography>
-        </label>
+        <Box {...getRootProps()} className={style.drop_file_input}>
+          <Box className={style.drop_file_input_label}>
+            {!isDragActive && (
+              <>
+                <MdOutlineCloudUpload
+                  style={{ transform: "translateY(17px)" }}
+                  color="#5569ff "
+                  fontSize="28px"
+                ></MdOutlineCloudUpload>
+                <Typography fontSize="13px" color="#223354" fontWeight="500">
+                  Drag drop files here
+                </Typography>
+              </>
+            )}
+            {isDragAccept && (
+              <>
+                <Box className={style.acceptSign}>
+                  <MdCheck color="white" fontSize="20px"></MdCheck>
+                </Box>
+                <Typography fontSize="13px" color="#223354" fontWeight="500">
+                  Drop the files to start uploading
+                </Typography>
+              </>
+            )}
+            {isDragReject && (
+              <>
+                <Box className={style.rejectSign}>X</Box>
+                <Typography fontSize="13px" color="#223354" fontWeight="500">
+                  You cannot upload these file types
+                </Typography>
+              </>
+            )}
+          </Box>
+          <input {...getInputProps()}></input>
+        </Box>
       </Box>
+
+      {file !== null && file !== undefined && (
+        <Box
+          sx={{
+            padding: "18px",
+            borderTop: "1px solid #d5d8dc",
+          }}
+        >
+          <Box className={` ${style.uploadSuccess}`}>
+            <MdOutlineCheckCircleOutline
+              color="#94dd72"
+              fontSize="22px"
+            ></MdOutlineCheckCircleOutline>
+            <Typography fontSize="14px" marginLeft="10px">
+              You have uploaded <span style={{ fontWeight: "700" }}>1 </span>
+              files!
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: "20px",
+            }}
+          >
+            <Typography fontSize="14px" color="#223354">
+              {file?.name}
+            </Typography>
+            <Typography fontSize="14px" color="#223354" fontWeight="600">
+              {file?.size} bytes
+            </Typography>
+          </Box>
+        </Box>
+      )}
     </PreviewCard>
   );
 }
